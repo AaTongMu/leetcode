830c1c7c2bd11d439b072cd7b80f5487656e256f
De.wey <de.wey@foxmail.com>
2016-12-18 22:27:44 +0800

    Modify the update README.md flow and create a solution file

--- loadQ.js
+++ loadQ.js
@@ -10,81 +10,35 @@ const fs = require('fs');
 const https = require('https');
 const algorithmApi = 'https://leetcode.com/api/problems/algorithms/';
 const algorithmPrefix = 'https://leetcode.com/problems/';
-
 const args = process.argv.slice(2);
+
 if (!args.length) {
     colorLog('Missing question_id!', 'error');
     process.exit();
 }
 const questionId = args[0];
 
-const algPromise = new Promise((resolve, reject) => {
-    // get algorithm list
-    let algorithmStr = '';
-    https.get(algorithmApi, (res) => {
-        res.on('data', (chunk) => {
-            algorithmStr += chunk;
-        }).on('end', () => {
-            colorLog('Get algorithm data success!', 'success');
-            resolve(JSON.parse(algorithmStr));
+/**
+ * Get question information 
+ * 
+ * @param {(number | string)} qId question_id
+ * @return Promise.
+ */
+function getQuestionInfo(url) {
+    return new Promise((resolve, reject) => {
+        // get algorithm list
+        let result = '';
+        https.get(url, (res) => {
+            res.on('data', (chunk) => {
+                result += chunk;
+            }).on('end', () => {
+                resolve(result);
+            });
+        }).on('error', (e) => {
+            reject(e);
         })
-    }).on('error', (e) => {
-        reject(e);
-    })
-});
-
-algPromise.then((algorithm) => {
-    // find question stat by question_id
-    let stat = null;
-    algorithm.stat_status_pairs.forEach((item) => {
-        if (item.stat.question_id == questionId) {
-            stat = item.stat;
-            switch (item.difficulty) {
-                case 2:
-                    stat.level = 'Medium'
-                    break;
-                case 3:
-                    stat.level = 'Hard'
-                    break;
-                default:
-                    stat.level = 'Easy'
-            }
-        }
-    })
-
-    return stat;
-}).then((stat) => {
-    if (!stat) {
-        colorLog('Question not found!', 'error');
-        process.exit();
-    }
-
-    const humpTitle = humpName(stat.question__title);
-
-    // update README.md
-    // TODO: check is README.md has this question id
-    let readmeStr = `\n|${stat.question_id}| [${stat.question__title}](${algorithmPrefix + stat.question__title_slug}) | [javascript](./algorithm/${humpTitle}.js) | ${stat.level} |`;
-    fs.appendFile('README.md', readmeStr, (err) => {
-        if (err) throw err;
-        colorLog('Update README.md success!', 'success');
     });
-
-    // create solution file
-    const filePath = './algorithm/' + humpTitle + '.js';
-    try {
-        const fileStat = fs.statSync(filePath);
-        colorLog(`File: ${humpTitle}.js is exist!`, 'warn');
-    } catch (e) {
-        fs.writeFile(filePath, '// solution', (err) => {
-            if (err) throw err;
-            colorLog(`Created ${filePath} success!`, 'success');
-        });
-    }
-
-}).catch((e) => {
-    colorLog(e.message, 'error');
-    process.exit();
-})
+}
 
 /**
  * Convert to hump named
@@ -136,3 +90,70 @@ function colorLog(text, level) {
 
     console.log('\x1B[' + LEVEL[level] + 'm%s\x1B[0m\n', text);
 }
+
+getQuestionInfo(algorithmApi).then((algorithmList) => {
+    // find question stat by question_id
+    if (!algorithmList) {
+        colorLog('No information was obtained on the question!', 'warn');
+        process.exit(); 
+    }
+
+    try {
+        algorithmList = JSON.parse(algorithmList);
+    } catch (e) {
+        colorLog(e.message, 'error');
+        process.exit();
+    }
+
+    let stat = null;
+    algorithmList.stat_status_pairs.forEach((item) => {
+        if (item.stat.question_id == questionId) {
+            stat = item.stat;
+            switch (item.difficulty) {
+                case 2:
+                    stat.level = 'Medium'
+                    break;
+                case 3:
+                    stat.level = 'Hard'
+                    break;
+                default:
+                    stat.level = 'Easy'
+            }
+        }
+    });
+
+    return stat;
+}).then((stat) => {
+    if (!stat) {
+        colorLog('Question not found!', 'error');
+        process.exit();
+    }
+
+    const humpTitle = humpName(stat.question__title);
+
+    // update README.md
+    // TODO: check is README.md has this question id
+    let readmeStr = `\n|${stat.question_id}| [${stat.question__title}](${algorithmPrefix + stat.question__title_slug}) | [javascript](./algorithm/${humpTitle}.js) | ${stat.level} |`;
+    fs.appendFile('README.md', readmeStr, (err) => {
+        if (err) throw err;
+        colorLog('Update README.md success!', 'success');
+    });
+
+    // create solution file
+    const filePath = './algorithm/' + humpTitle + '.js';
+    try {
+        const fileStat = fs.statSync(filePath);
+        colorLog(`File: ${humpTitle}.js is exist!`, 'warn');
+    } catch (e) {
+        fs.writeFile(filePath, '/** solution */', (err) => {
+            if (err) throw err;
+            colorLog(`Created ${filePath} success!`, 'success');
+        });
+    }
+}).catch((e) => {
+    colorLog(e.message, 'error');
+    process.exit();
+});
+
+
+
